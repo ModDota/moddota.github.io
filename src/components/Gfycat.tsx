9f029d4c5f8ea69dbe5aacd312770108aab7e3cd
ark120202 <ark120202@gmail.com>
2020-05-28 14:20:03 +0000

    Add new Introduction page

--- src/components/Gfycat.tsx
+++ src/components/Gfycat.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function Gfycat({ id, aspectRatio = 4 / 3 }: { id: string; aspectRatio: number }) {
     return (
-        <div style={{ position: "relative", paddingBottom: `${(1 / aspectRatio) * 100}%` }}>
+        <p style={{ position: "relative", paddingBottom: `${(1 / aspectRatio) * 100}%` }}>
             <iframe
                 src={`https://gfycat.com/ifr/${id}`}
                 scrolling="no"
@@ -12,6 +12,6 @@ export function Gfycat({ id, aspectRatio = 4 / 3 }: { id: string; aspectRatio: n
                 height="100%"
                 style={{ position: "absolute", top: 0, left: 0 }}
             />
-        </div>
+        </p>
     );
 }
--- src/components/YouTube.tsx
+++ src/components/YouTube.tsx
@@ -2,15 +2,15 @@ import React from "react";
 
 export function YouTube({ id, aspectRatio = 16 / 9 }: { id: string; aspectRatio: number }) {
     return (
-        <div style={{ position: "relative", paddingBottom: `${(1 / aspectRatio) * 100}%` }}>
+        <p style={{ position: "relative", paddingBottom: `${(1 / aspectRatio) * 100}%` }}>
             <iframe
                 src={`https://www.youtube.com/embed/${id}`}
                 frameBorder="0"
-                allowFullScreen={false}
+                allowFullScreen={true}
                 width="100%"
                 height="100%"
                 style={{ position: "absolute", top: 0, left: 0 }}
             />
-        </div>
+        </p>
     );
 }
