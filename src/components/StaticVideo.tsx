db5d0658cf0563310be4f3463948ddf38cfe42c8
Perryvw <perry.wesel@gmail.com>
2020-12-06 20:30:42 +0100

    Fix staticvideo function signature

--- src/components/StaticVideo.tsx
+++ src/components/StaticVideo.tsx
@@ -1,7 +1,7 @@
 import useBaseUrl from "@docusaurus/useBaseUrl";
 import React from "react";
 
-export function StaticVideo(path: string) {
+export function StaticVideo({ path }: { path: string }) {
     return (
         <video width="100%" height="100%" autoPlay muted loop>
             <source src={useBaseUrl(path)} type="video/mp4"></source>
