b9aee818d01d1472683fe3620722b279fa5c3bac
Pavel Djundik <xPaw@users.noreply.github.com>
2020-12-24 14:45:56 +0200

    space

--- api/src/components/Author.tsx
+++ api/src/components/Author.tsx
@@ -14,9 +14,9 @@ const TextMessageAuthor = styled.div`
 export function Author() {
   return (
     <TextMessageAuthor>
-      This page was created by <b>ark120202</b> who unfortunately has passed away on 29th November, 2020.
+      This page was created by <b>ark120202</b> who unfortunately has passed away on 29th November 2020 at the age of 18.
       <br /><br />
-      ark120202 was a pillar and champion of this community, he spent countless hours helping others,developing tools, and selflessly sharing his brilliance and intelligence to all that asked for help.
+      ark120202 was a pillar and champion of this community, he spent countless hours helping others, developing tools, and selflessly sharing his brilliance and intelligence to all that asked for help.
       <br /><br />
       You will always be remembered. — ModDota Community
     </TextMessageAuthor>
