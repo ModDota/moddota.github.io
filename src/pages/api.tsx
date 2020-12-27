018c0a9a0ffb5510484885a63a7fa946b1350af2
Pavel Djundik <xPaw@users.noreply.github.com>
2020-12-27 11:50:53 +0200

    more deps

--- src/pages/api.tsx
+++ src/pages/api.tsx
@@ -1,27 +1,61 @@
-import React from "react";
-import { Switch, Route, Redirect } from '@docusaurus/router'
-import styled from "styled-components";
+import React, { useEffect } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { NavBar } from "../../api/src/components/layout/NavBar";
+import { themeModdotaDark, themeModdotaLight } from "../../api/src/components/Themes";
+import { AppRoutes } from "../../api/src/pages";
+import { GlobalStyle } from "../../api/src/components/GlobalStyle";
+import { AppContext } from "../../api/src/components/AppContext";
+import Layout from '@theme/Layout';
 
-const EventsPage = React.lazy(() => import("../../api/src/pages/events"));
-const VScriptsPage = React.lazy(() => import("../../api/src/pages/vscripts"));
-
-const ErrorPage = styled.div`
-  margin: auto;
-  text-align: center;
-  font-size: 24px;
+const AppWrapper = styled.div`
+  display: flex;
+  flex-flow: column;
+  height: 100%;
+  background-color: ${(props) => props.theme.background};
+  color: ${(props) => props.theme.text};
 `;
 
-export function AppRoutes() {
+const PageContent = styled.div`
+  display: flex;
+  flex: 1;
+  min-height: 0;
+`;
+
+function App() {
+  const [darkmode, setDarkmode] = React.useState(false);
+
+  useEffect(() => {
+    const themeName = window.localStorage.getItem("theme");
+    setDarkmode(
+      themeName === "dark" || (themeName === null && window.matchMedia("(prefers-color-scheme: dark)").matches),
+    );
+  });
+
+  const appContext = {
+    darkmode,
+    setDarkmode(dark: boolean) {
+      window.localStorage.setItem("theme", dark ? "dark" : "");
+      setDarkmode(dark);
+    },
+  };
+
   return (
-    <Switch>
-      <Route path="/" exact>
-        <Redirect to="/vscripts" />
-      </Route>
-      <Route path="/events/:scope?" component={EventsPage} />
-      <Route path="/vscripts/:scope?" component={VScriptsPage} />
-      <Route>
-        <ErrorPage>404 Not Found</ErrorPage>
-      </Route>
-    </Switch>
+    <Layout title="API">
+    <AppContext.Provider value={appContext}>
+      <ThemeProvider theme={darkmode ? themeModdotaDark : themeModdotaLight}>
+        <AppWrapper>
+          <GlobalStyle />
+            <NavBar />
+            <PageContent>
+              <React.Suspense fallback={null}>
+                <AppRoutes />
+              </React.Suspense>
+            </PageContent>
+        </AppWrapper>
+      </ThemeProvider>
+    </AppContext.Provider>
+    </Layout>
   );
 }
+
+export default App;
